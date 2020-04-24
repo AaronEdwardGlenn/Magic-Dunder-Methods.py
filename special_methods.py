b0f349b278244c61ec342e81d580e085b1551f29
Aaron Edward Glenn <53454869+AaronEdwardGlenn@users.noreply.github.com>
2020-04-24 15:07:36 -0700

    initializing repr and str to modify built-in prit outs of class intance objects

--- special_methods.py
+++ special_methods.py
@@ -1,4 +1,8 @@
-# Let's review special methods
+# Let's review special methods(magic methods)
+# These allow us to emulate some of the built in behavior in python. it's also how we impliment operator overloading.
+# the __ double underscores that we find everywhere in python are called dunders.
+
+
 class Employee:
     raise_amount = 1.04
 
@@ -14,7 +18,7 @@ class Employee:
     def apply_raise(self):
         self.pay = int(self.pay * Employee.raise_amount)
 
-    def __repr__(self):
+    def __repr__(self):  # this is needed when we have the str that follows this. otherwise it will just default to repr. having it as pass is all that is needed
         pass
 
     def __str__(self):
@@ -23,3 +27,12 @@ class Employee:
 
 emp_1 = Employee('Aaron', 'Glenn', 100)
 emp_2 = Employee('Test', 'User', 200)
+
+print(1+2)
+print('a'+'b')
+
+# this doesn't print out anything useful. our special methods are going to change this.
+print(emp_1)
+
+repr(emp_1)  # repr is info on an object for devlopers
+str(emp_1)  # str is info that will be displayed for users and stuff
